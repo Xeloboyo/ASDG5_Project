493e34d337d6550d3ceea648635f776b1f7fa222
Xelo <williamchang435@gmail.com>
2021-08-18 16:50:15 +1000

    Add css file

--- client/src/Test.test.js
+++ client/src/Test.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import index from './index'
 
-it('renders without crashing', () => {
+it('App renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
