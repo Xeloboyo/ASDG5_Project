0bf6516a3c954d8b491c11bd63d89a705601ae96
Xelo <williamchang435@gmail.com>
2021-08-18 16:54:30 +1000

    removed import xd

--- client/src/Test.test.js
+++ client/src/Test.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import index from './index'
 
 it('App renders without crashing', () => {
   const div = document.createElement('div');
