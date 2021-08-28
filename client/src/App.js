1243d23ecebf0486c56cb06668557d5dc66c30c8
Xelo <williamchang435@gmail.com>
2021-08-29 00:53:40 +1000

    Homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,29 @@
 import "./App.css";
-import React, { useEffect } from "react";
+import React, { Component, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavigationBar from "./Component/NavigationBar";
+import Homepage from "./Component/Home/Homepage";
+import Container from 'react-bootstrap/Container';
 require("dotenv").config(); //
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <NavigationBar />
-      </Switch>
-    </Router>
-  );
+class App extends Component{
+  render(){
+
+    return(
+      <Router>
+        <Container className="mx-0 px-0" fluid>
+          <NavigationBar />
+          <Switch>
+            <Route path="/">
+              <Homepage />
+            </Route>
+          </Switch>
+        </Container>
+      </Router>
+    );
+
+  }
 }
+
 
 export default App;
--- client/src/Component/NavigationBar.js
+++ client/src/Component/NavigationBar.js
@@ -1,40 +1,60 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Form from "react-bootstrap/Form";
+import Button from 'react-bootstrap/Button';
+import FormControl from "react-bootstrap/FormControl";
+import { LinkContainer } from 'react-router-bootstrap';
+import ReactLogo from './logo.svg';
+
 function NavigationBar() {
   return (
-    <div>
-      <Link to="/">
-        <div className="header__option">
-          <span className="header__optionLineOne">Home</span>
-        </div>
-      </Link>
-      <Link to="/restaurants">
-        <div className="header__option">
-          <span className="header__optionLineOne">Restaurants</span>
-        </div>
-      </Link>
-      <Link to="/reviews">
-        <div className="header__option">
-          <span className="header__optionLineOne">Reviews</span>
-        </div>
-      </Link>
-      <Link to="/communityPage">
-        <div className="header__option">
-          <span className="header__optionLineOne">Community Page</span>
-        </div>
-      </Link>
-      <Link to="/login">
-        <div className="header__option">
-          <span className="header__optionLineOne">Login</span>
-        </div>
-      </Link>
-      <Link to="/register">
-        <div className="header__option">
-          <span className="header__optionLineOne">Register</span>
-        </div>
-      </Link>
-    </div>
+    <Navbar sticky="top"  className="bg-dark py-2 flex-grow-1">
+      <Container  className="bg-dark px-5 mx-0 d-flex">
+        <LinkContainer to="/">
+          <Navbar.Brand  className="text-white">
+            <img
+              src={ReactLogo}
+              height="50"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
+        </LinkContainer>
+        <Nav className=" flex-grow-1">
+          <Form className="d-flex mx-3">
+            <FormControl
+              type="search"
+              placeholder="Search"
+              className="mr-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+          <LinkContainer to="/">
+            <Nav.Link href="#home"  className="text-white  line-left">Home</Nav.Link>
+          </LinkContainer>
+          
+          <LinkContainer to="/restaurants">
+            <Nav.Link href="#features"  className="text-white line-left">Restaurants</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/reviews">
+            <Nav.Link href="#pricing"  className="text-white line-left">Reviews</Nav.Link>
+          </LinkContainer>
+        </Nav>
+        <Nav>
+          <LinkContainer to="/register" className="float-right mx-3">
+            <Button variant="outline-success">Register</Button>
+          </LinkContainer>
+          <LinkContainer to="/login"  className="float-right">
+            <Button variant="light">Login</Button>
+          </LinkContainer>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 }
 
