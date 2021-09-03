ca133df04b24494c9df574308d4a5fbc30bf3ef5
Ricky Felix <rickyfelix19@outlook.com>
2021-09-03 15:42:48 +1000

    create Cards component

---
+++ client/src/components/cards/Cards.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+import { IconContext } from 'react-icons';
+import { FaCheckCircle } from 'react-icons/fa';
+
+export const Cards = () => {
+  return (
+    <IconContext.Provider>
+      value ={{ color: 'green', size: '50px' }}
+      <div>
+        <Card>
+          <Card.Header>Takeaway Order</Card.Header>
+          <Card.Body>
+            <Card.Title>Number # </Card.Title>
+            <Card.Text>Description</Card.Text>
+            {/* <Card.Text>Description</Card.Text> */}
+          </Card.Body>
+          <Button variant="success" size="lg">
+            Confirm <FaCheckCircle />
+          </Button>
+        </Card>
+        ));
+      </div>
+    </IconContext.Provider>
+  );
+};
+
+export default Card;
