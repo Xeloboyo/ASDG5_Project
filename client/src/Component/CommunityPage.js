e0e6d4d04128846bb23966706f4a18794e25c053
lilylilyngo <13605973@student.uts.edu.au>
2021-09-03 14:48:10 +1000

    created form for community page

--- client/src/Component/CommunityPage.js
+++ client/src/Component/CommunityPage.js
@@ -1,10 +1,41 @@
 import React from "react";
+import Container from "react-bootstrap/esm/Container";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import FloatingLabel from "react-bootstrap/esm/FloatingLabel";
+import "./CommunityPage.css";
 
 function CommunityPage() {
   return (
-    <div>
-      <h1>hellp</h1>
-    </div>
+<Container className="center">
+    <Container className="mx-0 px-0" fluid >
+      <Form >
+        <Form.Group className="mb-3" controlId="formTitle">
+          <Form.Label>Title</Form.Label>
+          <Form.Control type="title" placeholder="Enter Title" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Categories</Form.Label>
+          <FloatingLabel controlId="floatingSelectGrid" label="Choose Category">
+            <Form.Select aria-label="Floating label select example">\
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </Form.Select>
+          </FloatingLabel> 
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Description</Form.Label>
+          <Form.Control as="textarea" rows={4} />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+    </Container>
+
   );
 }
 
