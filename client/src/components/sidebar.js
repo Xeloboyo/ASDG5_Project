8111a25baa51972726ca938a091890ccacb2a343
Ricky Felix <rickyfelix19@outlook.com>
2021-09-05 12:13:51 +1000

    create static sidebar (without router)

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -1,34 +1,74 @@
 import React from 'react';
-import { ListGroup } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+// import { ListGroup } from 'react-bootstrap';
+import {
+  ProSidebar,
+  SidebarHeader,
+  SidebarFooter,
+  SidebarContent,
+  Menu,
+  MenuItem,
+  SubMenu
+} from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+import {
+  IoTicket,
+  IoAnalytics,
+  IoPerson,
+  IoSettings,
+  IoHelpCircle
+} from 'react-icons/io';
 
 const Sidebar = () => {
   return (
-    <ListGroup>
-      <ListGroup.Item action href="#">
-        Tickets
-        {/* See your takeaway Ticket */}
-      </ListGroup.Item>
+    <ProSidebar>
+      <SidebarHeader>
+        {/* You can add a header for the sidebar ex: logo */}
+        Your Business Dashboard
+      </SidebarHeader>
 
-      <ListGroup.Item action href="#">
-        Analytics
-        {/* Check your Restaurant Analytics */}
-      </ListGroup.Item>
+      <SidebarContent>
+        {/* You can add the content of the sidebar ex: menu, profile details, ... */}
+        <Menu>
+          <MenuItem>
+            <IoTicket />
+            Tickets
+            <Link to="/" />
+            {/* See your takeaway Ticket */}
+          </MenuItem>
 
-      <ListGroup.Item action href="#">
-        Profile
-        {/* Change your Profile */}
-      </ListGroup.Item>
+          <MenuItem>
+            <IoAnalytics />
+            Analytics
+            <Link to="/" />
+            {/* Check your Restaurant Analytics */}
+          </MenuItem>
 
-      <ListGroup.Item action href="#">
-        Settings
-        {/* Change your Settings */}
-      </ListGroup.Item>
+          <MenuItem>
+            <IoPerson />
+            Profile
+            <Link to="/" />
+            {/* Change your Profile */}
+          </MenuItem>
 
-      <ListGroup.Item action href="#">
-        Help
-        {/* How to use, etc */}
-      </ListGroup.Item>
-    </ListGroup>
+          <MenuItem>
+            <IoSettings />
+            Settings
+            <Link to="/" />
+            {/* Change your Settings */}
+          </MenuItem>
+
+          <MenuItem>
+            <IoHelpCircle />
+            Help
+            <Link to="/" />
+            {/* How to use, etc */}
+          </MenuItem>
+        </Menu>
+      </SidebarContent>
+
+      <SidebarFooter>2021, Tangle &copy;</SidebarFooter>
+    </ProSidebar>
   );
 };
 
