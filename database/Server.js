0526e7b0ba56f265c7d1eee8d8b1821aae5b11f5
lilylilyngo <13605973@student.uts.edu.au>
2021-09-18 20:43:00 +1000

    connected mongodb

--- database/Server.js
+++ database/Server.js
@@ -1,13 +1,20 @@
 require("./config/db");
 
+require("./models/PostCommunity");
+
 const app = require("express")();
 
-const port = 3001;
+const port = 5002;
+
+const UserRouter = require("./api/CommunityPageForm");
 
 // For accepting post form data
+// eslint-disable-next-line import/order
 const bodyParser = require("express").json;
 
 app.use(bodyParser());
+
+app.use("/post", UserRouter);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
