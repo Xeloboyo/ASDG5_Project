c04d55cf768e95d98b0cefcd0fcd03e64aff645e
Ricky Felix <rickyfelix19@outlook.com>
2021-09-23 15:23:06 +1000

    i guess thats how you make a calendar lmao (time to take a break)

---
+++ client/src/components/Analytics/Calendar.js
@@ -0,0 +1,20 @@
+import Calendar from 'react-calendar';
+import Clock from 'react-clock';
+import TimeRangePicker from '@wojtekmaj/react-timerange-picker';
+import DateRangePicker from '@wojtekmaj/react-daterange-picker';
+
+// Use `onChange` prop for getting new values.
+// reference on states: https://reactpatterns.com/#state-hoisting
+
+import React from 'react';
+
+const Calendar = () => {
+  return (
+    <div>
+      <Calendar />
+      {/* lmao */}
+    </div>
+  );
+};
+
+export default Calendar;
