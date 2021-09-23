f4651da1438593c17e4c9e8caedf19ffb9618152
Ricky Felix <rickyfelix19@outlook.com>
2021-09-23 15:01:56 +1000

    change ticket layout

--- client/src/components/StaffTicket/StaffTicket.jsx
+++ client/src/components/StaffTicket/StaffTicket.jsx
@@ -2,27 +2,37 @@ import React, { Component } from 'react';
 import '../../scss/style.scss';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import Ticket from './Ticket';
+import StaffFooter from './StaffFooter';
 
 class StaffTicket extends Component {
   render() {
     return (
       <div
-        style={
-          {
-            //   background: 'lightgrey',
-            //   height: 'auto'
-            //   position: 'fixed'
-          }
-        }
+        style={{
+          background: 'lightgrey',
+          paddingTop: '85px',
+          width: '100%',
+          height: '100%',
+          position: 'absolute',
+          margin: 'auto'
+        }}
       >
         <Container>
           <div
-            style={{ float: 'right', marginTop: '100px', marginBottom: '50px' }}
+            style={{
+              float: 'right'
+              // marginTop: '80px',
+            }}
           >
             <Button variant="danger">Cancel All</Button>
           </div>
           <div>
-            <Ticket />
+            <div>
+              <Ticket />
+            </div>
+          </div>
+          <div>
+            <StaffFooter />
           </div>
         </Container>
       </div>
