2675c3d93ba083751569e7f655ad64f62271edc0
Ricky Felix <rickyfelix19@outlook.com>
2021-09-26 12:55:24 +1000

    change calendar package note: package is in vue, added babel and vuera to make it work in React

--- client/src/components/Analytics/Calendar.js
+++ client/src/components/Analytics/Calendar.js
@@ -1,7 +1,5 @@
-import Calendar from 'react-calendar';
 import Clock from 'react-clock';
-import TimeRangePicker from '@wojtekmaj/react-timerange-picker';
-import DateRangePicker from '@wojtekmaj/react-daterange-picker';
+import { Calendar } from 'v-calendar'; // this is a Vue package
 
 // Use `onChange` prop for getting new values.
 // reference on states: https://reactpatterns.com/#state-hoisting
@@ -11,6 +9,7 @@ import React from 'react';
 const Calendar = () => {
   return (
     <div>
+      <Clock />
       <Calendar />
       {/* lmao */}
     </div>
