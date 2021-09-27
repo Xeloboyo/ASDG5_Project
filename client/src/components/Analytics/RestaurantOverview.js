c3d34c9866dbb10c030922e3593a38bae8e6cd68
Ricky Felix <rickyfelix19@outlook.com>
2021-09-27 15:59:46 +1000

    remove nodemon duplicate

---
+++ client/src/components/Analytics/RestaurantOverview.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+
+const RestaurantOverview = () => {
+  return <div></div>;
+};
+
+export default RestaurantOverview;
