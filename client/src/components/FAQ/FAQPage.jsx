5e60c58cf0717f9452d6cd417582faa7956c15b2
Ricky Felix <rickyfelix19@outlook.com>
2021-10-04 00:50:59 +1100

    start new feature lmao, is it too late lmaooo

---
+++ client/src/components/FAQ/FAQPage.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Container, Row, Col, Card, Table } from 'react-bootstrap';
+
+export default class FAQPage extends Component {
+  render() {
+    return <div>FAQ Page</div>;
+  }
+}
