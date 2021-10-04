c0c8a152728837b12b986e7198b81118fdf70850
Ricky Felix <rickyfelix19@outlook.com>
2021-10-04 21:07:18 +1100

    refactoring database

--- database/Server.js
+++ database/Server.js
@@ -2,10 +2,10 @@ require('./config/db');
 
 const cors = require('cors');
 
-const app = require("express")(); 
-const mongoose = require("mongoose");
+const app = require('express')();
+const mongoose = require('mongoose');
 
-const port = process.env.PORT || 5002;
+// const port = process.env.PORT || 5002;
 
 // For accepting post form data
 // eslint-disable-next-line import/order
@@ -22,6 +22,7 @@ const PromotionsRouter = require('./api/PromotionsForm');
 
 app.use('/promotions', PromotionsRouter);
 app.use('/post', CommunityPostRouter);
+// app.use('/post', UserRouter);
 
 // ricky features
 require('./models/Ticket');
@@ -32,16 +33,14 @@ const AnalyticsRouter = require('./api/analyticsActions');
 
 app.use('/dashboard/ticket', TicketRouter);
 app.use('/dashboard/analytics', AnalyticsRouter);
-app.use(cors());
+// app.use(cors());
 
 // ismail features
-require("./models/User");
-const UserSchemaCopy = require("./models/User");
-const Routes = require("./api/Register"); //
+require('./models/User');
+const UserSchemaCopy = require('./models/User');
+const Routes = require('./api/Register'); //
 
-app.use("/register", Routes);
-
-app.use("/post", UserRouter); 
+app.use('/register', Routes);
 
 // const userSchema = new mongoose.Schema({
 //   User_Email: String,
@@ -50,19 +49,19 @@ app.use("/post", UserRouter);
 
 // const User = new mongoose.model("User", userSchema);
 
-app.post("/login", (req, res) => {
-  console.log(req.body)
-  const { User_Email,  User_Password } = req.body;
-  UserSchemaCopy.findOne({ User_Email: User_Email }, (err, user) => {
-    if (user) {
-      if (User_Password === user.User_Password) {
-        res.json({ message: "login success", user: user });
-      } else {
-        res.json({ message: "wrong credentials" });
-      }
-    }
-  });
-});
+// app.post('/login', (req, res) => {
+//   console.log(req.body);
+//   const { User_Email, User_Password } = req.body;
+//   UserSchemaCopy.findOne({ User_Email }, (err, user) => {
+//     if (user) {
+//       if (User_Password === user.User_Password) {
+//         res.json({ message: 'login success', user });
+//       } else {
+//         res.json({ message: 'wrong credentials' });
+//       }
+//     }
+//   });
+// });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
