fce03460b8eb4309822ea4d5dfd3a41e9a059f27
lilylilyngo <13605973@student.uts.edu.au>
2021-10-06 15:05:15 +1100

    clean server

--- database/Server.js
+++ database/Server.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const cors = require('cors');
-//const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+
 /* eslint-disable no-unused-vars */
 /* eslint-disable camelcase */
 require("./config/db");
 
 // configures to access dotenv environment
-require('dotenv').config();
+require("dotenv").config();
 
 // create express app
-const app = express();
+
 const port = process.env.PORT || 5002;
-const cors = require("cors");
 
 const app = require("express")();
 require("./models/User");
@@ -25,12 +24,14 @@ app.use(express.json());
 require("./config/db");
 require("./models/PostCommunity");
 
-// restaurant 
+// restaurant
 const restaurantRouter = require("./api/RestaurantForm");
-app.use('/restaurant', restaurantRouter);
+
+app.use("/restaurant", restaurantRouter);
 // menu
 const menuRouter = require("./api/MenuForm");
-app.use('/menu', menuRouter);
+
+app.use("/menu", menuRouter);
 const CommunityPostRouter = require("./api/CommunityPageForm");
 const PromotionsRouter = require("./api/PromotionsForm");
 
@@ -38,9 +39,8 @@ const Routes = require("./api/Register"); //
 
 const Route = require("./api/Login"); //
 
-// const RouteDelete = require("./api/Delete"); 
+// const RouteDelete = require("./api/Delete");
 
-const cors = require("cors"); //
 const UserSchemaCopy = require("./models/User");
 
 // const userSchema = new mongoose.Schema({
@@ -53,8 +53,8 @@ const UserSchemaCopy = require("./models/User");
 // For accepting post form data
 // eslint-disable-next-line import/order
 const bodyParser = require("express").json;
-app.use(bodyParser());
 
+app.use(bodyParser());
 
 app.use(cors());
 app.use(bodyParser());
@@ -65,7 +65,7 @@ app.use("/promotions", PromotionsRouter);
 app.use("/post", CommunityPostRouter);
 app.use(cors());
 
-app.use("/register", Routes);//
+app.use("/register", Routes); //
 
 app.post("/login", (req, res) => {
   console.log(req.body);
@@ -82,22 +82,6 @@ app.post("/login", (req, res) => {
     }
   });
 });
-
-//delete user
-app.delete("/delete", (req, res) => {
-  console.log(req.body)
-  const { User_Email } = req.body;
-  UserSchemaCopy.findOne({ User_Email: User_Email }, (err, user) => {
-    if (user) {
-      if (User_Email === user.User_Email) {
-        res.json({ message: "deleted", user: user });
-      } else {
-        res.json({ message: "error" });
-      }
-    }
-  });
-});
-
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
