b6c45f3e6b36bd18e3a8a7ad1ae3bcdb5d4156f2
Ricky Felix <rickyfelix19@outlook.com>
2021-10-20 22:28:40 +1100

    change analytics model and schema

--- database/models/Analytics.js
+++ database/models/Analytics.js
@@ -2,95 +2,49 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-// create TicketSchema
-const AnalyticsSchema = new Schema(
-  {
-    SubscriptionID: {
-      // foreign_key
-      type: Number
-    },
-
-    RestaurantID: {
-      // foreign_key
-      type: Number
-    },
-
-    // OrderID: {
-    //   // foreign_key
-    //   type: Number
-    // },
-
-    // PromotionID: {
-    //   // foreign_key
-    //   type: Number
-    // },
-
-    // PaymentID: {
-    //   // foreign_key
-    //   type: Number
-    // },
-
-    // ReservationID: {
-    //   // foreign_key
-    //   type: Number
-    // },
-
-    // TakeawayID: {
-    //   // foreign_key
-    //   type: Number
-    // },
-
-    // AnalyticsView: {
-    //   // view toggle
-    //   type: Boolean,
-    //   default: false // if not subscribe
-    // },
-
-    AnalyticsPageViewsNum: {
-      type: Number
-    },
-
-    AnalyticsShareNum: {
-      type: Number
-    },
-
-    AnalyticsRatingsNum: {
-      type: Number
-    },
-
-    AnalyticsBookingsNum: {
-      type: Number
-    },
-
-    AnalyticsTakeawayNum: {
-      type: Number
-    },
-
-    AnalyticsBookingAvg: {
-      type: String
-    },
-
-    AnalyticsTakeawayAvg: {
-      type: String
-    },
-
-    // AnalyticsPopularity: {
-    //   type: Number
-    // },
-
-    AnalyticsTakeawayProfit: {
-      type: Number
-    },
-
-    AnalyticsFilter: {
-      type: Date,
-      default: ''
-    }
+// create AnalyticsSchem
+const AnalyticsSchema = new Schema({
+  UsersTotal: {
+    type: Number
   },
-  {
-    timestamps: true
-  }
-);
+
+  RestaurantsTotal: {
+    type: Number
+  },
+
+  TrafficVisits: {
+    type: Number
+  },
+
+  CommPostsTotal: {
+    type: Number
+  },
+
+  Restaurant: [
+    {
+      Month: {
+        type: Date
+        // needs to return MM YYYY
+      },
+
+      Traffic: {
+        type: Number
+      },
+
+      Bookings: {
+        type: Number
+      },
+
+      ProfitTakeaway: {
+        type: Number
+      },
+
+      Takeaway: {
+        type: Number
+      }
+    }
+  ]
+});
 
 const Analytics = mongoose.model('Analytics', AnalyticsSchema);
 module.exports = Analytics;
